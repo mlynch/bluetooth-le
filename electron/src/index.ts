8ec25e1f84ae80b970d78372e84d4d4ba115cf5f
Max Lynch <max@ionic.io>
2021-05-16 15:23:00 -0500

    Plugin tweak

--- electron/src/index.ts
+++ electron/src/index.ts
@@ -1,11 +1,51 @@
-/*
-import { registerPlugin } from '@capacitor/core';
+import { WebPlugin } from '@capacitor/core';
+import { BleDevice, BluetoothLePlugin, BooleanResult, DeviceIdOptions, ReadOptions, ReadResult, RequestBleDeviceOptions, WriteOptions } from '../../src/definitions';
 
-import { BluetoothLe } from '../../src/definitions';
+export class BluetoothLeElectron extends WebPlugin implements BluetoothLePlugin {
+  async initialize(): Promise<void> {
+  }
+  async getEnabled(): Promise<BooleanResult> {
+    return {
+      value: true
+    }
+  }
+  async startEnabledNotifications(): Promise<void> {
+  }
+  async stopEnabledNotifications(): Promise<void> {
+  }
+  async requestDevice(_options?: RequestBleDeviceOptions): Promise<BleDevice> {
+    console.log('BLE ELECTRON: REQUESTING DEVICE');
 
-const Camera = registerPlugin<BluetoothLe>('Camera', {
-  web: () => import('./web').then(m => new m.CameraWeb()),
-});
-*/
+    return {
+      deviceId: 'fake'
+    }
+  }
+  async requestLEScan(_options?: RequestBleDeviceOptions): Promise<void> {
+  }
+  async stopLEScan(): Promise<void> {
 
-export * from './plugin';
+  }
+  async connect(_options: DeviceIdOptions): Promise<void> {
+  }
+  async createBond(_options: DeviceIdOptions): Promise<void> {
+  }
+  async isBonded(_options: DeviceIdOptions): Promise<BooleanResult> {
+    return {
+      value: false
+    }
+  }
+  async disconnect(_options: DeviceIdOptions): Promise<void> {
+  }
+  async read(_options: ReadOptions): Promise<ReadResult> {
+    return {
+    }
+  }
+  async write(_options: WriteOptions): Promise<void> {
+  }
+  async writeWithoutResponse(_options: WriteOptions): Promise<void> {
+  }
+  async startNotifications(_options: ReadOptions): Promise<void> {
+  }
+  async stopNotifications(_options: ReadOptions): Promise<void> {
+  }
+}
